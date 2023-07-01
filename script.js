c8ded8830241708b6548650dfa7d12a6ebfb0d09
Mateusz Leszczyński <bxrq@wp.pl>
2023-07-01 11:42:07 +0200

    add dedicated random function for every item

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 {
-    const weapons = ['sword','axe', 'mace', 'bow', 'crossbow', 'wand'];
-    const itemsTypes = ['wooden', 'bronze', 'steel', 'silver', 'golden'];
+    const weapons = ['sword', 'axe', 'mace', 'bow', 'crossbow', 'wand'];
+    const itemsTypes = ['clay', 'wooden', 'bronze', 'steel', 'silver', 'golden'];
+
+    //this code looks horrible; redability is really, really bad; how can I improve it?
 
     document.getElementById("equipmentButton").onclick = function () {
         const characterName = document.getElementById("characterName").value;
         const randomWeapon = Math.floor(Math.random() * weapons.length);
         const randomItemType = Math.floor(Math.random() * itemsTypes.length);
-        document.getElementById("characterWithEquipment").innerHTML = `Your character name is ${characterName}. <br> 
+        const randomItemTypeHelmet = Math.floor(Math.random() * itemsTypes.length);
+        const randomItemTypeArmor = Math.floor(Math.random() * itemsTypes.length);
+        const randomItemTypeLegs = Math.floor(Math.random() * itemsTypes.length);
+        const randomItemTypeBoots = Math.floor(Math.random() * itemsTypes.length);
+        document.getElementById("characterWithEquipment").innerHTML = `Your character name is ${characterName}. <br> <br>
         ${characterName} random items are: <br>
         ${itemsTypes[randomItemType]} ${weapons[randomWeapon]} <br>
-        ${itemsTypes[randomItemType]} helmet <br>
-        ${itemsTypes[randomItemType]} armor <br>
-        ${itemsTypes[randomItemType]} legs <br>
-        ${itemsTypes[randomItemType]} boots <br>
+        ${itemsTypes[randomItemTypeHelmet]} helmet <br>
+        ${itemsTypes[randomItemTypeArmor]} armor <br>
+        ${itemsTypes[randomItemTypeLegs]} legs <br>
+        ${itemsTypes[randomItemTypeBoots]} boots <br>
         `;
     }
 }
