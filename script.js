07f3b060d95f598de27176f329a9b0091f6aa190
Mateusz Leszczyński <bxrq@wp.pl>
2023-07-02 13:11:51 +0200

    add HTML structure and JS code for create character and random statistics for them

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
 {
-    const weapons = ['sword', 'axe', 'mace', 'bow', 'crossbow', 'wand'];
-    const itemsTypes = ['clay', 'wooden', 'bronze', 'steel', 'silver', 'golden'];
+    {
+        document.getElementById("characterButton").onclick = function () {
+            const characterName = document.getElementById("characterName").value;
+            document.getElementById("character").innerHTML = `Your character name is ${characterName}.`;
+        }
+    }
 
-    //this code looks horrible; redability is really, really bad; how can I improve it?
+    {
+        const weapons = ['sword', 'axe', 'mace', 'bow', 'crossbow', 'wand'];
+        const itemsTypes = ['clay', 'wooden', 'bronze', 'steel', 'silver', 'golden'];
 
-    document.getElementById("equipmentButton").onclick = function () {
-        const characterName = document.getElementById("characterName").value;
-        const randomWeapon = Math.floor(Math.random() * weapons.length);
-        const randomItemType = Math.floor(Math.random() * itemsTypes.length);
-        const randomItemTypeHelmet = Math.floor(Math.random() * itemsTypes.length);
-        const randomItemTypeArmor = Math.floor(Math.random() * itemsTypes.length);
-        const randomItemTypeLegs = Math.floor(Math.random() * itemsTypes.length);
-        const randomItemTypeBoots = Math.floor(Math.random() * itemsTypes.length);
-        document.getElementById("characterWithEquipment").innerHTML = `Your character name is ${characterName}. <br> <br>
-        ${characterName} random items are: <br>
-        ${itemsTypes[randomItemType]} ${weapons[randomWeapon]} <br>
-        ${itemsTypes[randomItemTypeHelmet]} helmet <br>
-        ${itemsTypes[randomItemTypeArmor]} armor <br>
-        ${itemsTypes[randomItemTypeLegs]} legs <br>
-        ${itemsTypes[randomItemTypeBoots]} boots <br>
-        `;
+        document.getElementById("equipmentButton").onclick = function () {
+            const characterName = document.getElementById("characterName").value;
+            const randomWeapon = Math.floor(Math.random() * weapons.length);
+            const randomItemType = Math.floor(Math.random() * itemsTypes.length);
+            const randomItemTypeHelmet = Math.floor(Math.random() * itemsTypes.length);
+            const randomItemTypeArmor = Math.floor(Math.random() * itemsTypes.length);
+            const randomItemTypeLegs = Math.floor(Math.random() * itemsTypes.length);
+            const randomItemTypeBoots = Math.floor(Math.random() * itemsTypes.length);
+
+            document.getElementById("characterWithEquipment").innerHTML = `
+                ${characterName} random items are: <br>
+                ${itemsTypes[randomItemType]} ${weapons[randomWeapon]} <br>
+                ${itemsTypes[randomItemTypeHelmet]} helmet <br>
+                ${itemsTypes[randomItemTypeArmor]} armor <br>
+                ${itemsTypes[randomItemTypeLegs]} legs <br>
+                ${itemsTypes[randomItemTypeBoots]} boots <br>
+            `;
+        }
+    }
+
+    {
+        document.getElementById("statisticsButton").onclick = function () {
+            const characterName = document.getElementById("characterName").value;
+            const randomAmountOfLuck = Math.floor(Math.random() * 7 + 1);
+            const randomAmountOfInsideFire = Math.floor(Math.random() * 3 + 1);
+
+            document.getElementById("characterWithStatistics").innerHTML = `
+                ${characterName} random statiscics are: <br>
+                ${randomAmountOfLuck} points of luck 🍀 <br>
+                ${randomAmountOfInsideFire} points of inside fire 🔥 <br>
+            `;
+
+            //TO DO: if random amount of luck or fire is 1 it must have 'point' instead of 'points' in HTML
+
+            // if (randomAmountOfLuck = 1) {
+            //     // document.getElementById("characterWithStatistics").innerHTML = `point`
+            // }
+        }
     }
 }
