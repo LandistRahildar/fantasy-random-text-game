f17f7033030dda4260d583f611c44893f601806f
Mateusz Leszczyński <bxrq@wp.pl>
2023-07-06 10:33:33 +0200

    add summmary button and code; some JS code refactoring (still looks really bad)

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 {
-    {
-        document.getElementById("characterButton").onclick = function () {
-            const characterName = document.getElementById("characterName").value;
-            document.getElementById("character").innerHTML = `Your character name is ${characterName}.`;
-        }
+    document.getElementById("characterButton").onclick = function () {
+        let characterName = document.getElementById("characterName").value;
+        document.getElementById("character").innerHTML = `Your character name is ${characterName}.`;
     }
 
-    {
-        const weapons = ['sword', 'axe', 'mace', 'bow', 'crossbow', 'wand'];
-        const itemsTypes = ['clay', 'wooden', 'bronze', 'steel', 'silver', 'golden'];
+    const weapons = ['sword', 'axe', 'mace', 'bow', 'crossbow', 'wand'];
+    const itemsTypes = ['clay', 'wooden', 'bronze', 'steel', 'silver', 'golden'];
 
-        document.getElementById("equipmentButton").onclick = function () {
-            const characterName = document.getElementById("characterName").value;
-            const randomWeapon = Math.floor(Math.random() * weapons.length);
-            const randomItemType = Math.floor(Math.random() * itemsTypes.length);
-            const randomItemTypeHelmet = Math.floor(Math.random() * itemsTypes.length);
-            const randomItemTypeArmor = Math.floor(Math.random() * itemsTypes.length);
-            const randomItemTypeLegs = Math.floor(Math.random() * itemsTypes.length);
-            const randomItemTypeBoots = Math.floor(Math.random() * itemsTypes.length);
+    document.getElementById("equipmentButton").onclick = function () {
+        const characterName = document.getElementById("characterName").value;
+        const randomWeapon = Math.floor(Math.random() * weapons.length);
+        const randomItemType = Math.floor(Math.random() * itemsTypes.length);
+        const randomItemTypeHelmet = Math.floor(Math.random() * itemsTypes.length);
+        const randomItemTypeArmor = Math.floor(Math.random() * itemsTypes.length);
+        const randomItemTypeLegs = Math.floor(Math.random() * itemsTypes.length);
+        const randomItemTypeBoots = Math.floor(Math.random() * itemsTypes.length);
 
-            document.getElementById("characterWithEquipment").innerHTML = `
+        document.getElementById("characterWithEquipment").innerHTML = `
                 ${characterName} random items are: <br>
                 ${itemsTypes[randomItemType]} ${weapons[randomWeapon]} <br>
                 ${itemsTypes[randomItemTypeHelmet]} helmet <br>
@@ -27,10 +24,7 @@
                 ${itemsTypes[randomItemTypeLegs]} legs <br>
                 ${itemsTypes[randomItemTypeBoots]} boots <br>
             `;
-        }
-    }
 
-    {
         document.getElementById("statisticsButton").onclick = function () {
             const characterName = document.getElementById("characterName").value;
             let randomAmountOfLuck = Math.floor(Math.random() * 6 + 2);
@@ -42,11 +36,19 @@
                 ${randomAmountOfInsideFire} points of inside fire 🔥 <br>
             `;
 
-            //TO DO: if random amount of luck or fire is 1 it must have 'point' instead of 'points' in HTML
-
-            // if (randomAmountOfLuck, randomAmountOfInsideFire = 1) {
-            //     document.getElementById("point").innerHTML = "point";
-            // }
+            document.getElementById("summaryButton").onclick = function () {
+                document.getElementById("characterSummary").innerHTML = `
+                ${characterName} is wearing 
+                ${itemsTypes[randomItemType]} ${weapons[randomWeapon]},
+                ${itemsTypes[randomItemTypeHelmet]} helmet,
+                ${itemsTypes[randomItemTypeArmor]} armor,
+                ${itemsTypes[randomItemTypeLegs]} legs and
+                ${itemsTypes[randomItemTypeBoots]} boots. <br>
+                ${characterName} also have basic 
+                ${randomAmountOfLuck} points of luck 🍀 and
+                ${randomAmountOfInsideFire} points of inside fire 🔥.
+                `;
+            }
         }
     }
 }
